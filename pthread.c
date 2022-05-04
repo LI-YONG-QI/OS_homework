784af2fe788acccd1ecba237b5d16bf168b66d9f
LI-YONG-QI <a2369028@gmail.com>
2022-05-04 17:56:30 +0800

    add pthread

--- pthread.c
+++ pthread.c
@@ -6,48 +6,45 @@
 #include <math.h>
 
 #define RADIUS 1
-#define N 10000
+#define N 100001
 
 
 // 子執行緒函數
-// void *runner (){
-//   srand()
-// }
+void *runner (){
 
-// double calculateDistance(double x, double y){
-  
-// }
+  srand(time(NULL));
+  int i;
+  int *result = malloc(sizeof(int) * 1);
+  result[0] = 0;
+
+  for(i = 0 ; i < N ; i++){
+    double x = (double) rand() / (RAND_MAX);
+    double y = (double) rand() / (RAND_MAX);
+    if(x * x + y * y < RADIUS){
+      result[0]++;
+    }
+  }
+  pthread_exit((void *) result); // 傳回結果
+}
+
 
 // 主程式
 int main() {
-  // pthread_t t; // 宣告 pthread 變數
-  // pthread_create(&t, NULL, runner, ""); // 建立子執行緒
+  void *ret1, *ret2, *ret3;
+  pthread_t t1; // 宣告 pthread 變數
+  pthread_t t2; // 宣告 pthread 變數
+  pthread_t t3; // 宣告 pthread 變數
+  pthread_create(&t1, NULL, runner, NULL); // 建立子執行緒
+  // pthread_create(&t2, NULL, runner, NULL); // 建立子執行緒
+  // pthread_create(&t3, NULL, runner, NULL); // 建立子執行緒
 
-  // // 主執行緒工作
-  // for(int i = 0;i < 3;++i) {
-  //   printf("Master\n"); // 每秒輸出文字
-  //   sleep(1);
-  // }
+  pthread_join(t1, &ret1); // 等待子執行緒執行完成
+  // pthread_join(t2, &ret2); // 等待子執行緒執行完成
+  // pthread_join(t3, &ret3); // 等待子執行緒執行完成
 
-  // pthread_join(t, NULL); // 等待子執行緒執行完成
+  int *sum = (int *) ret1 ;
 
-  srand(time(NULL));
-
-  int sum = 0;
-  double min = 0.0;
-  double max = 2*RADIUS;
-
-
-  int i;
-  for(i = 0 ; i < N ; i++){
-    double x = rand() / (RAND_MAX);
-    double y = rand() / (RAND_MAX);
-    if(x * x + y * y < RADIUS){
-      sum++;
-    }
-  }
-  
-  printf("PI = %f\n", (double) 4 * sum / (N - 1));
+  printf("PI = %f\n", (double) 4 * sum[0] / (N - 1));
 
   return 0;
 }
